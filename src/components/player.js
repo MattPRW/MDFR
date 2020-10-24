701947a826c894a52be58fb9d9d986d44faa6c2b
Matt Wilkie <matt@pamr.co.uk>
2020-10-24 08:55:24 +0100

    switched out radio.co embedded player for react-h5-audio-player, player component layout started

--- src/components/player.js
+++ src/components/player.js
@@ -1,10 +1,18 @@
 import React from "react"
 import ScriptTag from 'react-script-tag'
+import H5Player from 'react-h5-audio-player'
+import 'react-h5-audio-player/src/styles.scss'
+
+import playerStyles from "./player.module.scss" 
 
 const Player = () => {
   return (
     <div>
-      <ScriptTag  src="https://embed.radio.co/player/1ec874d.js" />
+      <div className= {playerStyles.container}>
+        <ScriptTag  className= {playerStyles.artwork} src="https://embed.radio.co/embed/s9e2b0d28b/artwork.js" />
+        <ScriptTag  src="https://embed.radio.co/embed/s9e2b0d28b/song.js" /> 
+        <H5Player src="https://s2.radio.co/s9e2b0d28b/listen"/>
+      </div>   
     </div>
   )
 }
